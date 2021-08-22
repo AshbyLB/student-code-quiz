ce9a46c1aa4108fbb9b0a22ad3deec5a71350f3d
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-08-22 19:52:52 -0400

    Creaded initial content for the JS page.  This includes the bones for the questions and the timer.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# student-code-quiz
-Code Quiz Homework!
+# Coding Quiz
+ 
+[Link to LIVE site.]()
+
+## Table of Contents
+- [Description](#Description)
+- [Usage](#Usage)
+- [Screenshot](#Screenshot)
+- [Languages](#Languages)
+
+## Discription
+
+
+## Usage 
+
+
+## Screenshot
+ 
+
+![me]()
+
+## Languages
+HTML and CSS
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,58 @@
-# Coding Quiz
- 
-[Link to LIVE site.]()
+var timer = document.getElementById("timer");
+var startButton = document.getElementById("start");
+var question = document.getElementById("question");
+var answers = document.querySelectorAll("li");
+var startQuiz = document.getElementById("start");
+var titlePage = document.getElementById("titlePage");
+var timeRemaining = 60;
 
-## Table of Contents
-- [Description](#Description)
-- [Usage](#Usage)
-- [Screenshot](#Screenshot)
-- [Languages](#Languages)
+let questions = [
+    {
+        question: "question",
+        answers: ["a1", "a2", "a3", "a4"],
+        correctAnswers: 0,
+    },
+    {
+        question: "question",
+        answers: ["a1", "a2", "a3", "a4"],
+        correctAnswers: 1,
+    },
+    {
+        question: "question",
+        answers: ["a1", "a2", "a3", "a4"],
+        correctAnswers: 2,
+    },
+    {
+        question: "question",
+        answers: ["a1", "a2", "a3", "a4"],
+        correctAnswers: 3,
+    },
+    {
+        question: "question",
+        answers: ["a1", "a2", "a3", "a4"],
+        correctAnswers: 0,
+    }]
 
-## Discription
+var startQuiz = document.getElementById("start");
+
+startQuiz.addEventListener("click", function () {
+    titlePage.style.display = "none";
+    setTimer();
+  for (i = 0; i < questions.length; i++) {
+      var element = questions[i];
+
+  }
+
+var setTimer = function () {
+    timer.textContent = "Time: " + timeRemaining;
+    var timeInterval = setInterval(function () {
+        timeRemaining--;
+        timer.textContent = "Time: " + timeRemaining;
+        if (timeRemaining === 0) {
+            clearInterval(timeInterval);
+            quizOver();
 
 
-## Usage 
-
-
-## Screenshot
- 
-
-![me]()
-
-## Languages
-HTML and CSS
+        }
+    }, 1000);
+}
