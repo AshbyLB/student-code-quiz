2909f933183a4f1a0b92d861b58155c4e33f695c
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-08-25 09:32:58 -0400

    Update to the hor loop to pull scores and names from the localstorage

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,10 +1,11 @@
 var userScore = JSON.parse(localStorage.getItem("user-score"));
 var ulEl = document.querySelector("ul");
+console.log(userScore);
 
 
 
 for (i = 0; i < userScore.length; i++){
     var getScore = document.createElement("li");
-    getScore.textContent = userScore[i];
+    getScore.textContent = userScore[i].name + userScore[i].score;
     ulEl.appendChild(getScore);
 }
